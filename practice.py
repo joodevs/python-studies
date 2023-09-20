c2fb498e5f2efb1782c91ce5554408bf912d16d7
Jooyoung Oh <jooyoungoh@Jooyoungs-MBP.local>
2023-09-20 15:53:59 +0900

    Added solutions

--- practice.py
+++ practice.py
@@ -118,4 +118,109 @@ print('%X'%n)
 
 #29 16진 정수 입력받아 8진수로 출력하기
 n = int(input(), 16)
-print('%')
+print('%o' %n)
+
+#30 영문자 1개 입력받아 10진수로 변환하기
+n = ord(input())
+print(n)
+
+#31 정수 입력받아 유니코드 문자로 변환하기
+c = int(input())
+print(chr(c))
+
+#32 정수 1개 입력받아 부호 바꾸기
+n = int(input())
+print(-n)
+
+#33 문자 1개 입력받아 다음 문자 출력하기
+n = ord(input())
+print(chr(n+1))
+
+#34 정수 2개 입력받아 차 계산하기
+a, b = map(int, input().split())
+c = a - b
+print(c)
+
+#35 실수 2개 입력받아 곱 계산하기
+f1, f2 = map(float, input().split())
+m = f1 * f2
+print(m)
+
+#36 단어 여러 번 출력하기
+w, n = input().split()
+print(w*int(n))
+
+#37 문장 여러 번 출력하기
+n = input()
+s = input()
+print(int(n)*s)
+
+#38 정수 2개 입력받아 거듭제곱 계산하기
+a, b = map(int, input().split())
+c = a**b
+print(c)
+
+#39 실수 2개 입력받아 거듭제곱 계산하기
+f1, f2 = map(float, input().split())
+m = f1**f2
+print(m)
+
+#40 정수 2개 입력받아 나눈 몫 계산하기
+a, b = map(int, input().split())
+print(a//b)
+
+#41 정수 2개 입력받아 나눈 나머지 계산하기
+a, b = map(int, input().split())
+print(a%b)
+
+#42 실수 1개 입력받아 소숫점이하 자리 변환하기
+a = float(input())
+print(format(a, ".2f"))
+
+#43 실수 2개 입력받아 나눈 결과 계산하기
+f1, f2 = map(float, input().split())
+print(format(f1/f2, ".3f"))
+
+#44 정수 2개 입력받아 자동 계산하기
+a, b = map(int, input().split())
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a%b)
+print(format(a/b, ".2f"))
+
+#45 정수 3개 입력받 합과 평균 출력하기
+a, b, c = map(int, input().split())
+sum = a + b + c
+avg = sum / 3
+print(sum, format(avg, ".2f"))
+
+#46 정수 1개 입력받아 2배 곱해 출력하기
+n = int(input())
+print(n<<1)
+
+#47 2의 거듭제곱 배로 곱해 출력하기
+a, b = map(int, input().split())
+print(a<<b)
+
+#48 정수 2개 입력받아 비교하기 1
+a, b = map(int, input().split())
+if a < b:
+    print(True)
+else:
+    print(False)
+
+#49 정수 2개 입력받아 비교하기 2
+a, b = map(int, input().split())
+if a == b:
+    print(True)
+else:
+    print(False)
+
+#50 정수 2개 입력받아 비교하기 3
+a, b = map(int, input().split())
+if a <= b:
+    print(True)
+else:
+    print(False)
